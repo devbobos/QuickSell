3af166ec5c266b9c7097f892d090c4221ce69ce2
손보광 <devbobos@humuson.com>
2021-04-08 17:59:54 +0900

    오류 수정

--- app/src/main/java/io/github/devbobos/quicksell/view/SplashActivity.kt
+++ app/src/main/java/io/github/devbobos/quicksell/view/SplashActivity.kt
@@ -1,12 +1,12 @@
 package io.github.devbobos.quicksell.view
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import io.github.devbobos.quicksell.R
 import kotlinx.coroutines.*
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash_activity)
